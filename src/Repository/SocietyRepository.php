fc21f5d3ffe8ff36ee1dd3e9da68866491a2985a
NichoSeb2 <NichoSeb2@outlook.fr>
2022-01-26 13:41:38 +0100

    added product, society and user entity

---
+++ src/Repository/SocietyRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Society;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+/**
+ * @method Society|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Society|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Society[]    findAll()
+ * @method Society[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SocietyRepository extends ServiceEntityRepository implements PasswordUpgraderInterface {
+    public function __construct(ManagerRegistry $registry) {
+        parent::__construct($registry, Society::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void {
+        if (!$user instanceof Society) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    // /**
+    //  * @return Society[] Returns an array of Society objects
+    //  */
+    /*
+    public function findByExampleField($value) {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Society {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
